b6d8744feeca98d26713d0c305c3a65ab190551f
twDFayet <damien.fayet@think-webdesign.ch>
2014-07-23 14:39:53 +0200

    Modification du titre de page #94

--- wp-content/themes/hardt/header.php
+++ wp-content/themes/hardt/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width" />
-                <title><?php wp_title( '|', true, 'right' ); ?></title>
+                <title><?php wp_title('|','true','right'); ?> | <?php bloginfo('name'); ?></title>
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri();?>/js/html5shiv.min.js"></script>
 		<![endif]-->
